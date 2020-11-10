1e22decb53b0834e26eba3697e6b56d9d00fda98
iljucha <ilja@surfer-172-29-27-173-wlan-login.oszimt.de>
2020-11-10 12:14:40 +0100

    opti: autorefresh for posts

--- examples/app/views/home.js
+++ examples/app/views/home.js
@@ -2,14 +2,23 @@
 import Post from "../components/post.js"
 import { Div } from "../components/generic.js"
 import * as service from "../services/db.js"
+import { loadingCircle } from "../components/decorators.js"
 import Flex from "../components/flex.js"
 
-export default async function Home() {
-    let posts = await service.getPosts(post => post)
-    let component = Flex({ direction: "column", gap: "8px" },
-        Div({ className: "post-container "},
-            ...posts.map(post => Post(post))
-        )
-    )
+export default async function Home() {    
+    const component = Flex({ direction: "column", gap: "8px" },
+        loadingCircle(Div({ key: 0, className: "post-container" })).addSignal("refresh", node => {
+            node.while(async () => {
+                let posts = await service.getPosts(post => post)
+                node.nativeElement.innerHTML = ""
+                node.append(...posts.map(post => Post(post)))
+            })
+        }).signal("refresh")
+    ).addSignal("refresh", node => node.child(0).signal("refresh"))
+
+    let interval = setInterval(() => {
+        component.signal("refresh")
+    }, 10000)
+
     return component
 }
