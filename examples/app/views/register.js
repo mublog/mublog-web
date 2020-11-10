c0249c92f1f512766eaaf2d30f228801f4619e60
iljucha <ilja@localhost.localdomain>
2020-11-10 16:04:28 +0100

    extend view: register

--- examples/app/views/register.js
+++ examples/app/views/register.js
@@ -1,15 +1,32 @@
 // @ts-check
 import i18n from "../lang/de_DE.js"
 
-import Box, { Seperator, Title, Input } from "../components/box.js"
+import Box, { Seperator, Title, Input, Button, Label } from "../components/box.js"
+import Form from "../components/form.js"
+import Flex from "../components/flex.js"
 
 export default async function Register() {
     let component = Box({ id: "register" },
-        Title({ }, i18n.createAccount),
+        Title({}, i18n.createAccount),
         Seperator(),
-        Input({ 
-            placeholder: "placeholder"
-        })
+        Form({},
+            Flex({},
+                Label({ labelText: i18n.alias }, 
+                    Input({ placeholder: i18n.alias })
+                ),
+                Label({ labelText: i18n.name }, 
+                    Input({ placeholder: i18n.name })
+                )
+            ),
+            Flex({},
+                Label({ labelText: i18n.password }, Input({ placeholder: i18n.password, type: "password" })),
+                Label({ labelText: i18n.password }, Input({ placeholder: i18n.password, type: "password" }))
+            ),
+            Seperator(),
+            Flex({ gap: "8px", direction: "row" },
+                Button({ type: "submit" }, i18n.register)
+            )
+        )
     )
     return component
 }
