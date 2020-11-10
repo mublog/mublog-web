0a1f802bb95ee170d1803877354a0f0521e8b82b
iljucha <ilja@surfer-172-29-27-173-wlan-login.oszimt.de>
2020-11-10 09:07:54 +0100

    removed JSX components.
    Vanilla Choc components will be used

--- examples/app.js
+++ examples/app.js
@@ -1,16 +1,13 @@
 // @ts-check
-/** @jsx Choc.create */
 import Choc from "../module.js"
 import Router from "./app/components/router.js"
 import Navigation from "./app/components/navigation.js"
 
-const App = (
-    <div id="app">
-        { Navigation }
-        <div id="content">
-            { Router }
-        </div>
-    </div>
+const App = Choc.create("div", { id: "app" },
+    Navigation,
+    Choc.create("div", { id: "content" },
+        Router
+    )
 )
 
 App.mount(document.getElementById("app"))
