3ca085da4af7bd736a45c0f77df125c19906faef
iljucha <ilja@localhost.localdomain>
2020-11-10 17:47:02 +0100

    notification component adding itself to the dom

--- examples/app.js
+++ examples/app.js
@@ -2,12 +2,10 @@
 import Choc from "../module.js"
 import Router from "./app/components/router.js"
 import Navigation from "./app/components/navigation.js"
-import Notification from "./app/components/notifications.js"
 
 const App = Choc.create("div", { id: "app" },
     Navigation,
     Choc.create("div", { id: "content" }, Router)
 )
 
-Notification.appendTo(document.body)
 App.mount(document.getElementById("app"))
