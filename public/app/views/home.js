b681a3179d5afe65ae3f715e82305bd6e2fa89f4
iljucha <ilja@surfer-172-29-27-173-wlan-login.oszimt.de>
2020-11-11 10:54:16 +0100

    add writer to home component

--- public/app/views/home.js
+++ public/app/views/home.js
@@ -1,31 +1,25 @@
 // @ts-check
 import Post from "../components/post.js"
 import { Div } from "../components/generic.js"
-import * as service from "../services/db.js"
 import { loadingCircle } from "../components/decorators.js"
 import Flex from "../components/flex.js"
+import Writer from "../components/writer.js"
 
-import http from "../services/http-client.js"
+import * as db from "../../fakedb.js"
 
-let posts = http.fetch("http://localhost:3000/posts", "GET")
-
-export default async function Home() {    
+export default async function Home() {
     const component = Flex({ direction: "column", gap: "8px" },
+        Writer(),
         loadingCircle(Div({ key: 0, className: "post-container" })).addSignal("refresh", node => {
             node.while(async () => {
-                let posts = (await http.fetch("http://localhost:3000/posts", "GET", {
-                    responseType: "JSON",
-                    body: undefined
-                })).content
+                let posts = db.Posts
                 node.nativeElement.innerHTML = ""
-                node.append(...posts.map(post => Post(post)))
+                node.append(...posts.map(Post))
             })
         }).signal("refresh")
     ).addSignal("refresh", node => node.child(0).signal("refresh"))
 
-    let interval = setInterval(() => {
-        component.signal("refresh")
-    }, 10000)
+    setInterval(() => component.signal("refresh"), 60000)
 
     return component
 }
