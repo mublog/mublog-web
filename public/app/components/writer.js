3232b2b076767a073ee97d7e7f255c01ef06c8e4
iljucha <ilja@surfer-172-29-27-173-wlan-login.oszimt.de>
2020-11-11 12:45:17 +0100

    add mark-down parser to writer

--- public/app/components/writer.js
+++ public/app/components/writer.js
@@ -5,6 +5,8 @@ import Flex from "./flex.js"
 import i18n from "../../lang/de_DE.js"
 import { Div, Icon } from "./generic.js"
 
+import translateMarkDown from "../helpers/mark-down.js"
+
 export default function Writer() {
     const textArea = Textarea({ placeholder: "Text" })
     const preview = Box({ className: "mark-down" })
@@ -16,13 +18,10 @@ export default function Writer() {
         preview,
         Seperator()
     )
-    const magnifier = Icon("magnifier", {
+    const magnifier = Icon("magnifier", { 
         className: "toggle-post-preview",
-        events: { 
-            click: togglePreview
-        }
+        title: i18n.showPostPreview
     })
-
     const component = Box({ className: "writer" },
         Form({}, 
             Div(null, textArea, magnifier),
@@ -33,6 +32,17 @@ export default function Writer() {
             )
         )       
     )
+
+    magnifier.addEvent("click", togglePreview)
+    textArea.node.addEvent("input", updatePreview)
+
+    function updatePreview() {
+        if (previewWrapper.get("_visible") === false) {
+            return
+        }
+        preview.nativeElement.innerHTML = translateMarkDown(textArea.ref.value)
+    }
+
     function togglePreview() {
         previewWrapper.style({ display: previewWrapper.get("_visible") === false ? "unset" : "none" })
         previewWrapper.set("_visible", previewWrapper.get("_visible") === false ? true : false)
---
+++ public/app/helpers/mark-down.js
@@ -0,0 +1,79 @@
+// @ts-check
+
+const IMG = /\![\[]{1}([^\]]+)[\]]{1}[\(]{1}([^\)\"]+)(\"(.+)\")?[\)]{1}/g
+const A = /[\[]{1}([^\]]+)[\]]{1}[\(]{1}([^\)\"]+)(\"(.+)\")?[\)]{1}/g
+const PRE0 = /^\s*\n\`\`\`(([^\s]+))?/gm
+const PRE1 = /^\`\`\`\s*\n/gm
+const PRE2 = /(\<pre.+\>)\s*\n\<p\>(.+)\<\/p\>/gm
+const CODE0 = /[\`]{1}([^\`]+)[\`]{1}/g
+const P0 = /^\s*(\n)?(.+)/gm
+const P1 = /\<(\/)?(h\d|ul|ol|li|img|pre)/
+const B = /[\*\_]{2}([^\*\_]+)[\*\_]{2}/g
+const I = /[\*\_]{1}([^\*\_]+)[\*\_]{1}/g
+const DEL = /[\~]{2}([^\~]+)[\~]{2}/g
+const UL0 = /^\s*\n\*/gm
+const UL1 = /^(\*.+)\s*\n([^\*])/gm
+const UL2 = /^\* (.+)/gm
+const OL0 = /^\s*\n\d\./gm
+const OL1 = /^(\d\..+)\s*\n([^\d\.])/gm
+const OL2 = /^\d\. (.+)/gm
+const H1 = /[\\#]{1} (.+)/g
+const H2 = /[\\#]{2} (.+)/g
+const H3 = /[\\#]{3} (.+)/g
+const H4 = /[\\#]{4} (.+)/g
+const H5 = /[\\#]{5} (.+)/g
+const H6 = /[\\#]{6} (.+)/g
+
+/**
+ * @param {string} text 
+ */
+export default function translateMarkDown(text) {
+    text = buildLists(text)
+    text = buildHeaders(text)
+    text = text.replace(IMG, `<img loading="lazy" src="$2" alt="$1" title="$4">`)
+    text = text.replace(A, `<a target="_blank" href="$2" title="$4">$1</a>`)
+    text = buildFontStyles(text)
+    text = text.replace(PRE0, `<pre class="$2">`)
+    text = text.replace(PRE1, "</pre>\n\n")
+    text = text.replace(CODE0, "<code>$1</code>")
+    text = text.replace(P0, m => P1.test(m) ? m : `<p>${m}</p>`)
+    text = text.replace(PRE2, "$1$2")
+    return text
+}
+
+/**
+ * @param {string} t 
+ */
+function buildHeaders(t) {
+    t = t.replace(H6, "<h6>$1</h6>")
+    t = t.replace(H5, "<h5>$1</h5>")
+    t = t.replace(H4, "<h4>$1</h4>")
+    t = t.replace(H3, "<h3>$1</h3>")
+    t = t.replace(H2, "<h2>$1</h2>")
+    t = t.replace(H1, "<h1>$1</h1>")
+    return t
+}
+
+/**
+ * @param {string} t 
+ */
+function buildFontStyles(t) {
+    t = t.replace(B, "<b>$1</b>") // **bold**, __bold__
+    t = t.replace(I, "<i>$1</i>") // *italic*, _italic_
+    t = t.replace(DEL, "<del>$1</del>") // ~~delete~~
+    return t
+}
+
+/**
+ * @param {string} t 
+ */
+function buildLists(t) {
+    t = t.replace(UL0, "<ul>\n*")
+    t = t.replace(UL1, "$1\n</ul>\n\n$2")
+    t = t.replace(UL2, "<li>$1</li>")
+    //ol
+    t = t.replace(OL0, "<ol>\n1.")
+    t = t.replace(OL1, "$1\n</ol>\n\n$2")
+    t = t.replace(OL2, "<li>$1</li>")
+    return t
+}
