7e9c0baaa0ef25eb381fb819068ec08a1a433ada
iljucha <ilja@surfer-172-29-27-173-wlan-login.oszimt.de>
2020-11-11 12:33:24 +0100

    add preview magnifier

--- public/app/components/writer.js
+++ public/app/components/writer.js
@@ -3,7 +3,7 @@ import Box, { Seperator, Textarea, Button } from "./box.js"
 import Form from "./form.js"
 import Flex from "./flex.js"
 import i18n from "../../lang/de_DE.js"
-import { Div } from "./generic.js"
+import { Div, Icon } from "./generic.js"
 
 export default function Writer() {
     const textArea = Textarea({ placeholder: "Text" })
@@ -16,9 +16,16 @@ export default function Writer() {
         preview,
         Seperator()
     )
+    const magnifier = Icon("magnifier", {
+        className: "toggle-post-preview",
+        events: { 
+            click: togglePreview
+        }
+    })
+
     const component = Box({ className: "writer" },
         Form({}, 
-            textArea,
+            Div(null, textArea, magnifier),
             Seperator(),
             previewWrapper,
             Flex({ gap: "8px" },
@@ -26,10 +33,9 @@ export default function Writer() {
             )
         )       
     )
-
-    textArea.addEvent("dblclick", (node, event) => {
+    function togglePreview() {
         previewWrapper.style({ display: previewWrapper.get("_visible") === false ? "unset" : "none" })
         previewWrapper.set("_visible", previewWrapper.get("_visible") === false ? true : false)
-    })
+    }
     return component
 }
