a73cb67c8e726ec0cd7fde99b138355c3d502fbe
iljushka <7289801-iljushka@users.noreply.gitlab.com>
2020-11-17 12:38:40 +0100

    app.js => app.ts

---
+++ public/app.ts
@@ -0,0 +1,16 @@
+import Doc, { mount } from "../modules/doc/module"
+import Router from "./app/components/router"
+import Header from "./app/components/header"
+import Navigation from "./app/components/navigation"
+import Notifications from "./app/components/notifications"
+
+const App = Doc.createNode("div", { id: "app" },
+    Header,
+    Notifications,
+    Doc.createNode("div", { id: "app-grid" },
+        Navigation,
+        Doc.createNode("div", { id: "content" }, Router)
+    )
+)
+
+mount(document.getElementById("app"), App)
