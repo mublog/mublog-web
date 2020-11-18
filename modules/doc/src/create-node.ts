2ce5efea05fcf05aa2e83b00c7db284897ee488b
iljushka <7289801-iljushka@users.noreply.gitlab.com>
2020-11-18 08:17:30 +0100

    renewed and commented

--- modules/doc/src/create-node.ts
+++ modules/doc/src/create-node.ts
@@ -1,11 +1,27 @@
-import type { HTMLProperties, MappedElement, State } from "./types"
+import type { HTMLProperties, MappedElement, State, Subscriber } from "./types"
 
-export default function createNode<Tag extends keyof HTMLElementTagNameMap>(
+/**
+ * Creates an HTMLElement that can be directly appended to the **DOM**.\
+ * The Element itself has no extra methods to stay lightweight.
+ * @param tag Tag of an HTMLElement from **HTMLElementTagNameMap**
+ * @param props Properties to set for the HTMLElement
+ * @param children Nodes children
+ * 
+ * @example
+ * let node = Doc.createElement("div", { id: "app", className: "node" },
+ *   Doc.createElement("span", null, "Hello World")
+ * )
+ */
+export default function createElement<Tag extends keyof HTMLElementTagNameMap>(
     tag: Tag, 
     props: Partial<HTMLProperties<Tag>> = {},
     ...children: any[]
 ): MappedElement<Tag> {
     if (typeof tag === "function") {
+        /**
+         * Doc.createNode can be used as a TSX/ JSX function.
+         * Therefore, the first real argument will be set by the component function.
+         */
         // @ts-expect-error
         return tag(props, children)
     }
@@ -15,11 +31,22 @@ export default function createNode<Tag extends keyof HTMLElementTagNameMap>(
     return el
 }
 
+/**
+ * Sets Properties for the Element and subscribes States if needed
+ * @param el HTMLElement Object
+ * @param props Every property you can imagine!
+ */
 function setProperties<Target extends Element>(el: Target, props: any) {
     for (const key in props) {
         if (props[key].isState === true) {
             const state: State<any> = props[key]
-            state.subscribe(val => el[key] = val)
+            const subscription = (val: any) => {
+                if (!el) {
+                    return state.unsubscribe(subscription)
+                }
+                el[key] = val
+            }
+            state.subscribe(subscription)
             el[key] = props[key].value
         }
         else {
@@ -28,51 +55,72 @@ function setProperties<Target extends Element>(el: Target, props: any) {
     }
 }
 
-function setChildren(el: Element, children: any[]) {
+/**
+ * @param el Parent HTMLElement
+ * @param children Children Elements
+ */
+function setChildren<Target extends Element>(el: Target, children: any[]) {
     children.forEach(child => appendChild(el, child))
 }
 
-function appendChild(el: Element, child: any) {
+/**
+ * Recursively appends Child Elements to the Parent Element.\
+ * The Child itself can be a Array, string, number or a State<Element | Element[] | string | number>
+ * @param el Parent HTMLElement
+ * @param child Single given child Element
+ */
+function appendChild<Target extends Element>(el: Target, child: any) {
+    /**
+     * Append recursively
+     */
     if (Array.isArray(child)) {
         child.forEach(subChild => appendChild(el, subChild))
     }
-    else if (typeof child === "string") {
-        el.appendChild(document.createTextNode(child))
+    /**
+     * Append TextNode
+     */
+    else if (typeof child === ("string" || "number")) {
+        el.appendChild(document.createTextNode(child + ""))
     }
+    /**
+     * Append your regular Element
+     */
     else if (child instanceof Element) {
         el.appendChild(child)
     }
+    /**
+     * This is going to be special
+     */
     else if (child.isState) {
+        /**
+         * First, we don't know what the children are,\
+         * but we will subscribe to the changes anyway,\
+         * there might be Elements waiting for us.
+         */
         if (Array.isArray(child.value)) {
-            let subscription = val => {
+            const state: State<any> = child
+            let subscription: Subscriber<any> = val => {
                 if (!el) {
-                    return child.unsubscribe(subscription)
+                    return state.unsubscribe(subscription)
                 }
                 el.innerHTML = ""
                 appendChild(el, val)
             }
-            child.subscribe(subscription)
+            state.subscribe(subscription)
         }
-        else if (child.value instanceof Element) {
-            el.appendChild(child.value)
-            let subscription = val => {
-                if (!el) {
-                    return child.unsubscribe(subscription)
-                }
-                el.innerHTML = ""
-                el.appendChild(val)
-            }
-            child.subscribe(subscription)
-        }
+        /**
+         * You wanted to subscribe to basic values? That's okay too!
+         */
         else if (typeof child.value === ("string" || "number")) {
-            let subDoc = document.createTextNode(String(child.value))
-            let subscription = val => {
+            const state: State<string | number> = child
+            let subDoc = document.createTextNode(state.value + "")
+            let subscription: Subscriber<string | number> = val => {
                 if (!el) {
-                    return child.unsubscribe(subscription)
+                    return state.unsubscribe(subscription)
                 }
-                subDoc.textContent = String(val)
+                subDoc.textContent = val + ""
             }
-            child.subscribe(subscription)
+            state.subscribe(subscription)
             el.appendChild(subDoc)
         }
     }
