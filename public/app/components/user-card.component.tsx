93d5da60c132bf4a3149584bb50ef6740b4b1f1c
iljucha <ilja@localhost.localdomain>
2020-12-01 15:04:17 +0100

    user-card->a

--- public/app/components/user-card.component.tsx
+++ public/app/components/user-card.component.tsx
@@ -2,6 +2,7 @@ import Doc, { usePortal } from "../../modules/doc/mod"
 import * as µ from "./mu.component"
 import adjustCardPosition from "../helpers/adjust-card-position"
 
+// @ts-expect-error
 const UserCardPortal = usePortal(UserCard)
 
 const users = [
@@ -16,6 +17,10 @@ async function UserCard({ alias, top, left }) {
   [top, left] = adjustCardPosition(top, left)
   let user = users.find(u => u.alias === alias)
 
+  if (!user) {
+    return document.createComment("oops")
+  }
+
   // styles={{ backgroundImage: `url(${user.image})` }}
 
   return (
@@ -25,7 +30,7 @@ async function UserCard({ alias, top, left }) {
       onmouseleave={UserCardPortal.close}
     >
       <µ.Header>
-        <div styles={{ display: "flex", gap: "8px", alignItems: "center" }}>
+        <a href={`/user/${alias}`} styles={{ display: "flex", gap: "8px", alignItems: "center" }}>
           <div className="user-image-wrap">
             <div className="user-image" />
             <div className="avatar-circle" />
@@ -38,7 +43,7 @@ async function UserCard({ alias, top, left }) {
               @{alias}
             </div>
           </div>
-        </div>
+        </a>
       </µ.Header>
       <µ.Box arrow="top-left">
         {user.bio}
