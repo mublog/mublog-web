dae7d1685660df9bf07035c2606249f91b18d7b5
iljushka <7289801-iljushka@users.noreply.gitlab.com>
2020-12-07 00:11:47 +0100

    update http.service with auto token

--- public/app/services/http.service.ts
+++ public/app/services/http.service.ts
@@ -1,8 +1,17 @@
 export declare interface HttpOptions {
   contentType?: "json" | "text" | "blob"
-  body?: any
   init?: RequestInit
 }
+
+const token = () => localStorage.getItem("token")
+
+const httpHeaders = (): RequestInit => ({
+  headers: {
+    "Content-Type": "application/json",
+    "Authorize": "Bearer " + token(),
+    "Authorization": "Bearer " + token()
+  }
+})
 
 async function toResponseType(response: Response, type: "json" | "text" | "blob") {
   switch (type) {
@@ -16,43 +25,36 @@ async function toResponseType(response: Response, type: "json" | "text" | "blob"
   }
 }
 
-function createInit(method: string, body?: any, options?: HttpOptions): RequestInit {
-  if (!options) {
-    options = {}
-  }
-  return { ...options.init, method, body }
+function createHttp(method: string, body: string, options: HttpOptions): [HttpOptions, RequestInit] {
+  if (!options) options = {}
+  if (!options.contentType) options.contentType = "json"
+  let init: RequestInit = { ...httpHeaders(), method, body }
+  return [options, init]
+}
+
+export async function method<Type>(url: string, method: string, body: any, options?: HttpOptions): Promise<[Type, Response]> {
+  let [opt, init] = createHttp(method, body, options)
+  let response = await fetch(url, init)
+  let data: Type = await toResponseType(response, opt.contentType)
+  return [data, response]
 }
 
 export async function get<Type>(url: string, options?: HttpOptions): Promise<[Type, Response]> {
-  let response = await fetch(url, options.init)
-  let data: Type = await toResponseType(response, options.contentType)
-  return [data, response]
+  return await method(url, "GET", null, options)
 }
 
 export async function post<Type>(url: string, body?: any, options?: HttpOptions): Promise<[Type, Response]> {
-  let init = createInit("POST", body, options)
-  let response = await fetch(url, init)
-  let data: Type = await toResponseType(response, options.contentType)
-  return [data, response]
+  return await method(url, "POST", body, options)
 }
 
 export async function put<Type>(url: string, body?: any, options?: HttpOptions): Promise<[Type, Response]> {
-  let init = createInit("PUT", body, options)
-  let response = await fetch(url, init)
-  let data: Type = await toResponseType(response, options.contentType)
-  return [data, response]
+  return await method(url, "PUT", body, options)
 }
 
 export async function del<Type>(url: string, body?: any, options?: HttpOptions): Promise<[Type, Response]> {
-  let init = createInit("DELETE", body, options)
-  let response = await fetch(url, init)
-  let data: Type = await toResponseType(response, options.contentType)
-  return [data, response]
+  return await method(url, "DELETE", body, options)
 }
 
 export async function patch<Type>(url: string, body?: any, options?: HttpOptions): Promise<[Type, Response]> {
-  let init = createInit("PATCH", body, options)
-  let response = await fetch(url, init)
-  let data: Type = await toResponseType(response, options.contentType)
-  return [data, response]
+  return await method(url, "PATCH", body, options)
 }
