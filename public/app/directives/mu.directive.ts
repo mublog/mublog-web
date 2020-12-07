1d8b21e4b426621332348bca31c8f8cab83e8923
iljushka <7289801-iljushka@users.noreply.gitlab.com>
2020-12-07 15:15:01 +0100

    shorten user-card-directive

--- public/app/directives/mu.directive.ts
+++ public/app/directives/mu.directive.ts
@@ -1,12 +1,6 @@
 import { onEvent, useDirective } from "../../mod/doc/mod"
 import UserCardPortal from "../components/user-card.component"
 
-useDirective("user-card", (el, prop: string) => {
-  onEvent(el, "mouseenter", event => {
-    UserCardPortal.open({
-      alias: prop,
-      top: event.clientY,
-      left: event.clientX
-    })
-  })
-})
+useDirective("user-card", (el, prop: string) => onEvent(el, "mouseenter", event => {
+  UserCardPortal.open({ alias: prop, top: event.clientY, left: event.clientX })
+}))
