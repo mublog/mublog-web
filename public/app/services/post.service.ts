39795405c063c700a3c972e1c0d4ca4334453196
iljushka <7289801-iljushka@users.noreply.gitlab.com>
2020-12-07 15:15:43 +0100

    simplify post-service

--- public/app/services/post.service.ts
+++ public/app/services/post.service.ts
@@ -1,19 +1,18 @@
 import { useState } from "../../mod/doc/mod"
 import * as http from "./http.service"
-import UserService from "./user.service"
 
 export default PostService()
 
 function PostService() {
   const API_URL = "http://localhost:5000/api/v1/posts"
-  const API_POSTS_ID = (id: number) => API_URL + "/" + id
-  const API_POSTS_LIKE = (id: number) => API_URL + "/like/" + id
+  const API_POSTS_ID = API_URL + "/"
+  const API_POSTS_LIKE = API_URL + "/like/"
 
   const posts = useState<PostModel[]>([])
   const pub = { getPosts, add, hasPost, load, like, localById }
 
   async function load(page: number = 1, size: number = 100) {
-    let [postRes, res] = await http.get<PostsResponse>(API_URL + `?page=${page}&size=${size}`)
+    let [postRes, res] = await http.get<ResponseWrapper<PostModel[]>>(API_URL + `?page=${page}&size=${size}`)
     if (res.status === 200) {
       posts.set(postRes.data)
     }
@@ -27,7 +26,7 @@ function PostService() {
   }
 
   async function getPost(id: number) {
-    let [wrapper, res] = await http.get<ResponseWrapper<PostModel>>(API_POSTS_ID(id))
+    let [wrapper, res] = await http.get<ResponseWrapper<PostModel>>(API_POSTS_ID + id)
     if (res.status === 200) return wrapper.data
   }
 
@@ -44,10 +43,10 @@ function PostService() {
   async function like(id: number) {
     let post = localById(id)
     let method = post.liked ? "del" : "post"
-    let [_, res] = await http[method]<ResponseWrapper<null>>(API_POSTS_LIKE(id), "{}")
+    let [_, res] = await http[method]<ResponseWrapper<null>>(API_POSTS_LIKE + id, "{}")
 
     if (res.status === 200) {
-      let [w2, r2] = await http.get<ResponseWrapper<PostModel>>(API_POSTS_ID(id))
+      let [w2, r2] = await http.get<ResponseWrapper<PostModel>>(API_POSTS_ID + id)
       if (r2.status === 200) {
         patchOne(id, w2.data)
         return true
