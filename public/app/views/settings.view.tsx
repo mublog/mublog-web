854f3b5b205bff05e8cabd6167928f7bf40fe688
iljushka <7289801-iljushka@users.noreply.gitlab.com>
2020-12-07 15:14:26 +0100

    update i18n

--- public/app/views/settings.view.tsx
+++ public/app/views/settings.view.tsx
@@ -1,5 +1,6 @@
 import Doc from "../../mod/doc/mod"
 import i18n from "../../lang/de_DE.json"
+import i18nProgressiveWebAppText from "../../lang/de_DE.progressiveWebAppText"
 import * as µ from "../components/mu.component"
 import InstallService from "../services/install.service"
 
@@ -9,17 +10,20 @@ export default async function SettingsView() {
       <µ.Box>
         <µ.Header>{i18n.userSettings}</µ.Header>
       </µ.Box>
+
       <µ.Box>
-        <µ.Header>{i18n.general}</µ.Header>
-        <div styles={{ display: "flex", gap: "8px", alignItems: "baseline" }}>
+        <µ.Header>{i18n.progressiveWebApp}</µ.Header>
+        <µ.Box>
+          {i18nProgressiveWebAppText}
+        </µ.Box>
+        <µ.Footer>
           <µ.Button if={InstallService.isNotInstalled} onclick={InstallService.install}>
             {i18n.installApp}
           </µ.Button>
           <µ.Button if={InstallService.isInstalled} onclick={InstallService.uninstall}>
             {i18n.uninstallApp}
           </µ.Button>
-          <span if={InstallService.isInstalled}>({i18n.isInstalled})</span>
-        </div>
+        </µ.Footer>
       </µ.Box>
     </div>
   ) as HTMLDivElement
---
+++ public/lang/de_DE.progressiveWebAppText.ts
@@ -0,0 +1,3 @@
+export default
+  `Durch die Installation dieser Anwendung werden einige Inhalte (wie z. B. Grafiken) lokal auf dem Gerät hinterlegt, damit diese
+  nicht mehr heruntergeladen werden müssen, wenn Sie diese Seite wieder besuchen.`
