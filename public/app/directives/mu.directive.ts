1cd91d05923f1339f765a12fc209ee6d3105fdfb
iljushka <7289801-iljushka@users.noreply.gitlab.com>
2020-12-08 21:34:08 +0100

    allow uploads add copytoclipboard directive

--- public/app/directives/mu.directive.ts
+++ public/app/directives/mu.directive.ts
@@ -1,6 +1,18 @@
 import { onEvent, useDirective } from "../../mod/doc/mod"
 import UserCardPortal from "../components/user-card.component"
+import copyToClipboard from "../helpers/copy-to-clipboard"
 
 useDirective("user-card", (el, prop: string) => onEvent(el, "mouseenter", event => {
   UserCardPortal.open({ alias: prop, top: event.clientY, left: event.clientX })
+}))
+
+useDirective("copyToClipboard", (el, prop: any) => onEvent(el, "click", () => {
+  let content: string
+  if (typeof prop === "function") {
+    content = prop()
+  }
+  else {
+    content = prop
+  }
+  copyToClipboard(content)
 }))
--- public/app/views/home.view.tsx
+++ public/app/views/home.view.tsx
@@ -1,13 +1,18 @@
 import Doc, { useRef } from "../../mod/doc/mod"
 import * as µ from "../components/mu.component"
 import i18n from "../../lang/de_DE.json"
+import { up } from "../helpers/up-down"
+import * as http from "../services/http.service"
+import { Uploaded, Uploads } from "../services/generic.service"
 import * as PostService from "../services/post.service"
-import UserService from "../services/user.service"
+import * as UserService from "../services/user.service"
 import Post from "../components/post.component"
 import NotificationService from "../services/notification.service"
+import { createKey } from "../../mod/doc/src/helper"
+import { base64ToBlob } from "../helpers/base64-to-blob"
 
 export default async function HomeView() {
-  PostService.load()
+  await PostService.load()
 
   return (
     <div styles={{ display: "flex", flexDirection: "column", gap: "8px" }}>
@@ -31,14 +36,50 @@ function HomeWriter() {
       <µ.Writer ref={WriterRef}>
         <div styles={{ display: "flex", gap: "8px" }}>
           <µ.Button type="submit">{i18n.send}</µ.Button>
-          <µ.Button type="button">{i18n.uploadImage}</µ.Button>
+          <µ.Button type="button" onclick={uploadImage}>{i18n.uploadImage}</µ.Button>
+        </div>
+        <div if={Uploaded}>
+          <µ.Seperator />
+          <div
+            styles={{ display: "flex", flexDirection: "column-reverse", gap: "8px" }}
+            for={{
+              of: Uploads,
+              do: (props: UploadItem<string>) => (
+                <µ.UploadItemElement key={props.key} fileData={props.fileData} fileName={props.fileName} />
+              )
+            }}
+          />
         </div>
       </µ.Writer>
     </form>
   ) as HTMLFormElement
 
+  async function uploadImage() {
+    let [file, error] = await up({ accept: "image/*", readAs: "DataURL" })
+    if (error) return NotificationService.push(null, error.message)
+    if (!file.type.startsWith("image/")) return NotificationService.push(null, file.type)
+    Uploads.update(images => images.push({ key: createKey(), fileName: file.name, fileData: file.data }))
+    const [blob] = await base64ToBlob(file.data)
+    const formData = new FormData()
+    formData.append("file", blob, file.name)
+
+    let [data, res] = await http.post<any>("/api/v1/media", formData, {
+      init: {
+        body: formData,
+        method: "POST",
+        headers: {
+          Authorize: "Bearer " + localStorage.getItem("token")
+        }
+      }
+    })
+    console.log({ data, res })
+  }
+
   function getValues() {
-    return WriterRef.current.getValues()
+    return {
+      text: WriterRef.current.getValues().raw,
+      images: Uploads.value().map(({ fileData, fileName }) => ({ name: fileName, data: fileData }))
+    }
   }
 
   async function tryPost(event: Event) {
@@ -49,13 +90,14 @@ function HomeWriter() {
     }
     const values = getValues()
 
-    if (!values?.raw) {
+    if (!values.text) {
       return NotificationService.push(null, i18n.messageCriteriaError)
     }
 
-    let success = await PostService.add(values.raw)
+    let success = await PostService.add(values.text)
     if (success) {
       WriterRef.current.clear()
+      Uploads.set([])
       PostService.load()
     }
   }
