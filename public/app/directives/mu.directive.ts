560743a8d770a77d995230ce287958a6301777f2
iljushka <7289801-iljushka@users.noreply.gitlab.com>
2020-12-09 21:18:16 +0100

    adjust directives add directive "tooltip"

--- public/app/directives/mu.directive.ts
+++ public/app/directives/mu.directive.ts
@@ -1,21 +1,29 @@
-import { onEvent, useDirective } from "../../mod/doc/mod"
+import Doc, { onEvent } from "../../mod/doc/mod"
 import UserCardPortal from "../components/user-card.component"
 import copyToClipboard from "../helpers/copy-to-clipboard"
+import i18n from "../../lang/de_DE.json"
 
-useDirective("user-card", (el, prop: string) => onEvent(el, "mouseenter", event => {
+Doc.registerDirective("user-card", (el, prop: string) => onEvent(el, "mouseenter", event => {
   UserCardPortal.open({ alias: prop, top: event.clientY, left: event.clientX })
 }))
 
-useDirective("copyToClipboard", (el, prop: any) => onEvent(el, "click", () => {
-  let content: string = ""
-  if (typeof prop === "function") {
-    let propR = prop()
-    if (typeof propR === "string") {
-      content = propR
+Doc.registerDirective("copyToClipboard", (el, prop: string | (() => string)) => {
+  el.title = i18n.clickToCopy.replace("$c", typeof prop === "function" ? prop() : prop)
+  onEvent(el, "click", () => {
+    let content: string
+    if (typeof prop === "function") {
+      content = prop()
     }
-  }
-  else if (typeof prop === "string") {
-    content = prop
-  }
-  copyToClipboard(content)
-}))
+    else if (typeof prop === "string") {
+      content = prop
+    }
+    if (content) {
+      copyToClipboard(content)
+    }
+  })
+})
+
+Doc.registerDirective("tooltip", (el, prop) => {
+  el.classList.add("tooltip")
+  el.appendChild(Doc.createElement("span", { className: "tooltip-text" }, prop))
+})
