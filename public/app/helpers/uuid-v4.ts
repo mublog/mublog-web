c55ad55b17624031082c9f0fed6bf0d20870026f
iljushka <7289801-iljushka@users.noreply.gitlab.com>
2021-01-02 14:16:41 +0100

    add uuid-v4 helper

---
+++ public/app/helpers/uuid-v4.ts
@@ -0,0 +1,9 @@
+const UUID_V4_TEMPLATE = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx"
+
+export default function UUIDv4(): string {
+  return UUID_V4_TEMPLATE.replace(/[xy]/g, c => {
+    let r = Math.random() * 16 | 0
+    let v = c === "x" ? r : (r & 0x3 | 0x8)
+    return v.toString(16)
+  })
+}
