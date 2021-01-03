29c20471ab7b07458683fb63de1fbd06811df986
iljushka <7289801-iljushka@users.noreply.gitlab.com>
2021-01-03 16:55:47 +0100

    add functionality: follow/unfollow show followers/ following

--- public/app/components/user-card.component.tsx
+++ public/app/components/user-card.component.tsx
@@ -1,4 +1,4 @@
-import Doc, { observable, portal, reference } from "../../mod/doc/mod"
+import Doc, { observable, portal } from "../../mod/doc/mod"
 import * as µ from "./mu.component"
 import adjustCardPosition from "../helpers/adjust-card-position"
 import * as UserService from "../services/user.service"
@@ -12,18 +12,19 @@ export async function UserCard({ alias, top, left }) {
   let exist = existing()
   if (exist) exist.remove()
 
+  let yourUserName = UserService.currentUser().username
   let user = await UserService.getUser(alias)
-  const FollowingReference = reference<µ.IconElement>()
-  const Following = observable("user-follow")
+  const followers = observable(0)
+  const following = observable(0)
+  const follows = observable(false)
+  const followsNot = observable(true)
+  follows.subscribe(value => followsNot.set(!value))
 
-  // styles={{ backgroundImage: `url(${user.image})` }}
+  await refreshFollowers()
+  await refreshFollowing()
 
   return (
-    <µ.Box
-      styles={{ top: `${top}px`, left: `${left}px` }}
-      id="user-card"
-      onmouseleave={UserCardPortal.close}
-    >
+    <µ.Box styles={{ top: `${top}px`, left: `${left}px` }} id="user-card" onmouseleave={UserCardPortal.close}>
       <µ.Header>
         <div styles={{ display: "flex", gap: "8px", alignItems: "center" }}>
           <div className="user-image-wrap">
@@ -31,26 +32,49 @@ export async function UserCard({ alias, top, left }) {
             <div className="avatar-circle" />
           </div>
           <a href={`/user/${alias}`} tooltip={i18n.visitUser.replace("$u", user.displayName)} styles={{ flex: "1" }}>
+
             <div>
-              {user.displayName}
-            </div>
-            <div>
-              @{alias}
+              @{user.username}
             </div>
           </a>
-          <µ.Icon
-            ref={FollowingReference}
-            name="user-follow"
-            className="cursor-pointer"
-            tooltip={i18n.follow}
-          />
+          <div className="cursor-pointer" if={alias !== yourUserName}>
+            <div if={UserService.isUser}>
+              <µ.Icon if={followsNot} tooltip={i18n.follow} name="user-follow" onclick={follow} />
+              <µ.Icon if={follows} tooltip={i18n.unfollow} name="user-unfollow" onclick={unfollow} />
+            </div>
+          </div>
         </div>
       </µ.Header>
+      <div>{i18n.followers}: {followers}</div>
+      <div>{i18n.following}: {following}</div>
       <µ.Box arrow="top-left" if={user.description.length > 0}>
-        {user.description}
+        <span >{user.description}</span>
       </µ.Box>
     </µ.Box>
   ) as HTMLElement
+
+  async function follow() {
+    await UserService.follow(user.username)
+    await refreshFollowers()
+    await refreshFollowing()
+  }
+
+  async function unfollow() {
+    await UserService.unfollow(user.username)
+    refreshFollowers()
+    refreshFollowing()
+  }
+
+  async function refreshFollowers() {
+    let list = await UserService.followers(alias)
+    followers.set(list.length)
+    follows.set(!!list.find(user => user.username === yourUserName))
+  }
+
+  async function refreshFollowing() {
+    let list = await UserService.following(alias)
+    following.set(list.length)
+  }
 }
 
 export default UserCardPortal
